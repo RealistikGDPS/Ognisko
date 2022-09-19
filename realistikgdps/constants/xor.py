f03904953ecb62d6a3abe7e3f42fb9a527dba059
RealistikDash <realistikdash@gmail.com>
2022-09-19 18:30:39 +0200

    implement gjp, use xor module

--- realistikgdps/constants/xor.py
+++ realistikgdps/constants/xor.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from enum import Enum
 
-# TODO: This shouldn't probably be a strenum since we use literally 0 of its
-# features.
-class XorKeys(str, Enum):
-    LEVEL_PASSWORD = "26364"
-    MESSAGE = "14251"
-    GJP = "37526"
-    QUESTS = "19847"
-    CHESTS = "59182"
+class XorKeys:
+    LEVEL_PASSWORD = b"26364"
+    MESSAGE = b"14251"
+    GJP = b"37526"
+    QUESTS = b"19847"
+    CHESTS = b"59182"
--- realistikgdps/usecases/hashes.py
+++ realistikgdps/usecases/hashes.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
 import asyncio
+import base64
 
 import bcrypt
+import xor_cipher
+
+from realistikgdps.constants.xor import XorKeys
 
 
 def compare_bcrypt(hashed: str, plain: str) -> bool:
@@ -22,3 +26,19 @@ async def compare_bcrypt_async(hashed: str, plain: str) -> bool:
     """
 
     return await asyncio.to_thread(compare_bcrypt, hashed, plain)
+
+
+def decode_gjp(gjp: str) -> str:
+    """Decodes the "Geometry Jump Password" format into plaintext.
+
+    Args:
+        gjp (str): The encoded GJP string.
+
+    Returns:
+        str: The plaintext password.
+    """
+
+    return xor_cipher.xor_cyclic(
+        content=base64.b64decode(gjp.encode()),
+        key=XorKeys.GJP,
+    ).decode()
