5520e2f56c75faf58fd76e63ad4c12f81cf783e4
RealistikDash <realistikdash@gmail.com>
2022-09-20 23:10:27 +0200

    authentication dependency and gdobj cleanups

--- realistikgdps/usecases/__init__.py
+++ realistikgdps/usecases/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from . import authentication
+from . import gd_obj
 from . import hashes
 from . import user_accounts
-from . import gd_obj
--- realistikgdps/usecases/gd_obj.py
+++ realistikgdps/usecases/gd_obj.py
@@ -1,9 +1,7 @@
+from __future__ import annotations
+
 from realistikgdps.typing.protocols import SupportsStr
 
-def into_gd_obj(obj: dict[SupportsStr, SupportsStr], sep: str = ":") -> str:
-    res = ""
 
-    for key, value in obj.items():
-        res += f"{key}:{value}:"
-    
-    return res[:-1]
+def into_gd_obj(obj: dict[SupportsStr, SupportsStr], sep: str = ":") -> str:
+    return sep.join(str(key) + sep + str(value) for key, value in obj.items())
