b82d2759d828961deeafc7cf03d92a7dfb905651
RealistikDash <realistikdash@gmail.com>
2022-09-24 00:44:37 +0200

    fix: missing friend status in gd object call

--- realistikgdps/api/profiles.py
+++ realistikgdps/api/profiles.py
@@ -10,9 +10,7 @@ from realistikgdps.usecases import gd_obj
 from realistikgdps.usecases import users
 from realistikgdps.usecases.users import authenticate_dependency
 
-# TODO: Maybe most logic should be moved to a `request_profile` usecase?
-# The usecase would handle the logic of checking if the user is allowed to
-# view the profile, friendship status, etc.
+
 async def view_user_info(
     target_id: int = Form(..., alias="targetAccountID"),
     user: User = Depends(authenticate_dependency()),
@@ -26,5 +24,5 @@ async def view_user_info(
     logger.info(f"Successfully viewed the profile of {target.user}.")
 
     return gd_obj.into_gd_obj(
-        gd_obj.create_gd_profile(target.user),
+        gd_obj.create_gd_profile(target.user, target.friend_status),
     )
