eb03159c692b8a4ffe616b5120a7732d5bc6ecb1
RealistikDash <realistikdash@gmail.com>
2022-09-25 01:11:37 +0200

    remove: unnecessary TYPE_CHECKING use

--- realistikgdps/state/repositories.py
+++ realistikgdps/state/repositories.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from realistikgdps.models.user import User
+from realistikgdps.models.user import User
 
 user_repo: dict[int, User] = {}
