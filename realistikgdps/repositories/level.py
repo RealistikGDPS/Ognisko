db21c29e6c99ff8a7f83a5f7ef6d501705bc4a63
RealistikDash <realistikdash@gmail.com>
2022-10-14 22:23:58 +0200

    add: level insert

--- realistikgdps/repositories/level.py
+++ realistikgdps/repositories/level.py
@@ -65,3 +65,54 @@ async def from_id(level_id: int) -> Optional[Level]:
         update_locked=level_db["update_locked"],
         deleted=level_db["deleted"],
     )
+
+
+async def create(level: Level) -> int:
+    return await services.database.execute(
+        "INSERT INTO levels (name, user_id, description, custom_song_id, "
+        "official_song_id, version, length, two_player, publicity, render_str, "
+        "game_version, binary_version, upload_ts, original_id, downloads, likes, "
+        "stars, difficulty, demon_difficulty, coins, coins_verified, requested_stars, "
+        "feature_order, search_flags, low_detail_mode, object_count, copy_password, "
+        "building_time, update_locked, deleted) VALUES (:name, :user_id, "
+        ":description, :custom_song_id, :official_song_id, :version, :length, "
+        ":two_player, :publicity, :render_str, :game_version, :binary_version, "
+        ":upload_ts, :original_id, :downloads, :likes, :stars, :difficulty, "
+        ":demon_difficulty, :coins, :coins_verified, :requested_stars, :feature_order, "
+        ":search_flags, :low_detail_mode, :object_count, :copy_password, "
+        ":building_time, :update_locked, :deleted)",
+        {
+            "name": level.name,
+            "user_id": level.user_id,
+            "description": level.description,
+            "custom_song_id": level.custom_song_id,
+            "official_song_id": level.official_song_id,
+            "version": level.version,
+            "length": level.length.value,
+            "two_player": level.two_player,
+            "publicity": level.publicity.value,
+            "render_str": level.render_str,
+            "game_version": level.game_version,
+            "binary_version": level.binary_version,
+            "upload_ts": level.upload_ts,
+            "original_id": level.original_id,
+            "downloads": level.downloads,
+            "likes": level.likes,
+            "stars": level.stars,
+            "difficulty": level.difficulty.value,
+            "demon_difficulty": level.demon_difficulty.value
+            if level.demon_difficulty is not None
+            else None,
+            "coins": level.coins,
+            "coins_verified": level.coins_verified,
+            "requested_stars": level.requested_stars,
+            "feature_order": level.feature_order,
+            "search_flags": level.search_flags.value,
+            "low_detail_mode": level.low_detail_mode,
+            "object_count": level.object_count,
+            "copy_password": level.copy_password,
+            "building_time": level.building_time,
+            "update_locked": level.update_locked,
+            "deleted": level.deleted,
+        },
+    )
