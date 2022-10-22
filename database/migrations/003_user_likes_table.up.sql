1b14d2553199d5aac9a13d44d985a566d23cb123
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-10-22 15:48:14 +0100

    fix: migration syntax

--- database/migrations/003_user_likes_table.up.sql
+++ database/migrations/003_user_likes_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user_likes` (
     `target_type` TINYINT(1) NOT NULL,
     `target_id` INT(11) NOT NULL,
     `user_id` INT(11) NOT NULL,
-    `value` tinyint(1) NOT NULL
+    `value` tinyint(1) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
