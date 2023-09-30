91ecb27456c2ed31c7eb77dc1739caa8586dbece
RealistikDash <realistikdash@gmail.com>
2023-09-30 21:42:19 +0100

    remove: duplicate STAR_PRIVILEGES definition

--- rgdps/constants/leaderboards.py
+++ rgdps/constants/leaderboards.py
@@ -8,12 +8,3 @@ class LeaderboardType(str, Enum):
     CREATOR = "creators"
     STAR_FRIENDS = "friends"
     STAR_RELATIVE = "relative"
-
-
-STAR_PRIVILEGES = (
-    UserPrivileges.USER_STAR_LEADERBOARD_PUBLIC | UserPrivileges.USER_PROFILE_PUBLIC
-)
-
-CREATOR_PRIVILEGES = (
-    UserPrivileges.USER_CREATOR_LEADERBOARD_PUBLIC | UserPrivileges.USER_PROFILE_PUBLIC
-)
