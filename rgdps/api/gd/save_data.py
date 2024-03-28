1ef7dcd77652e4bee954e18a6b976ffba1372984
RealistikDash <realistikdash@gmail.com>
2024-03-28 19:55:07 +0100

    fix: save files not working

--- rgdps/api/gd/save_data.py
+++ rgdps/api/gd/save_data.py
@@ -8,7 +8,7 @@ from fastapi.responses import FileResponse
 from rgdps import logger
 from rgdps.api import responses
 from rgdps.api.context import HTTPContext
-from rgdps.api.dependencies import password_authenticate_dependency
+from rgdps.api.dependencies import authenticate_dependency
 from rgdps.config import config
 from rgdps.constants.errors import ServiceError
 from rgdps.models.user import User
@@ -17,7 +17,7 @@ from rgdps.usecases import save_data
 
 async def save_data_get(
     ctx: HTTPContext = Depends(),
-    user: User = Depends(password_authenticate_dependency()),
+    user: User = Depends(authenticate_dependency()),
 ):
     # TODO: Streaming
     data = await save_data.get(ctx, user.id)
@@ -43,7 +43,7 @@ async def save_data_get(
 
 async def save_data_post(
     ctx: HTTPContext = Depends(),
-    user: User = Depends(password_authenticate_dependency()),
+    user: User = Depends(authenticate_dependency()),
     data: str = Form(..., alias="saveData"),  # Pain.
     game_version: int = Form(..., alias="gameVersion"),
     binary_version: int = Form(..., alias="binaryVersion"),
@@ -78,4 +78,4 @@ async def save_data_post(
 # An endpoint that specified which server to use for storing user save data.
 # TODO: Support an external save server.
 async def get_save_endpoint(request: Request) -> str:
-    return f"{request.url.scheme}://{request.url.hostname}{config.http_url_prefix}"
+    return f"https://{request.url.hostname}{config.http_url_prefix}"
