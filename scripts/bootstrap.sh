ca2643975b7b9deaacddc9ff1685b431dd37c56c
RealistikDash <realistikdash@gmail.com>
2024-08-13 13:49:30 +0100

    refactor: no need for await service

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,14 +6,6 @@ if [ -z "$APP_COMPONENT" ]; then
   exit 1
 fi
 
-echo "Waiting for services to become available..."
-
-SERVICE_READINESS_TIMEOUT=60
-./scripts/await_service.sh $SQL_HOST $SQL_PORT $SERVICE_READINESS_TIMEOUT
-./scripts/await_service.sh $REDIS_HOST $REDIS_PORT $SERVICE_READINESS_TIMEOUT
-./scripts/await_service.sh $MEILI_HOST $MEILI_PORT $SERVICE_READINESS_TIMEOUT
-
-
 ./scripts/ensure_sql.sh
 ./scripts/ensure_meili.sh
 ./scripts/migrate.sh up
